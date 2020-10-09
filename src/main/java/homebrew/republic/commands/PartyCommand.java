d9c815a7edc289c58152ecbf314d8bc1df4dc2ee
HoneyBlocks <springchimp404@gmail.com>
2020-10-08 22:39:56 -0500

    Party feature

---
+++ src/main/java/homebrew/republic/commands/PartyCommand.java
@@ -0,0 +1,34 @@
+package homebrew.republic.commands;
+
+import homebrew.republic.Party;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class PartyCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        Party party = new Party();
+        Player player = (Player) sender;
+        if(args.length < 1) {
+            sender.sendMessage(ChatColor.BLUE + "#--------------|Party Help|--------------#");
+            sender.sendMessage("/party join");
+            sender.sendMessage("/party create");
+            sender.sendMessage("/party leave");
+        }
+        switch(args[0]) {
+            case "join":
+                party.openGUI(player);
+                break;
+            case "create":
+                new PartyCreate(player, args);
+                break;
+            default:
+                sender.sendMessage(ChatColor.RED + "Not a valid command.");
+        }
+
+        return false;
+    }
+}
